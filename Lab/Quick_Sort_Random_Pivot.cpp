f1f6eb2497f8caf1420ec9f529c86bc7b991f66b
Mohammad Mehedi Hasan <90519550+mehedi807@users.noreply.github.com>
2023-08-03 11:50:19 +0600

    Create Quick_Sort_Random_Pivot.cpp

---
+++ Lab/Quick_Sort_Random_Pivot.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n = 5;
+int partition(int a[],int p, int r)
+{
+    int pivot = a[r];
+    int i = p-1;
+    for(int j=p; j<r; j++){
+        if(a[j] <= pivot){
+            i++;
+            swap(a[i],a[j]);
+        }
+    }
+    swap(a[i+1] , a[r]);
+    return i+1;
+}
+int random_pivot(int a[],int p,int r){
+    int i = p + rand() % (r-p);
+    swap(a[r],a[i]);
+    return partition(a,p,r);
+}
+void quick_sort(int a[],int p, int r)
+{
+    int q;
+    if(p<r){
+        q = random_pivot(a,p,r);
+        quick_sort(a,p,q-1);
+        quick_sort(a,q+1,r);
+    }
+}
+int main()
+{
+    int a[n] = {6,3,8,1,9};
+    quick_sort(a,0,n-1);
+    for(int i=0; i<n; i++){
+        cout << a[i] << " ";
+    }
+}
+
